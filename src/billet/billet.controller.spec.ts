511d206f1ef64bfc8f7d1b9d6a5094bbdbb8567d
Vinicius Santos <viniciusantos.florencio@gmail.com>
2024-05-30 14:12:21 -0300

    refactor: billet controller

--- src/billet/billet.controller.spec.ts
+++ src/billet/billet.controller.spec.ts
@@ -5,6 +5,21 @@ import { Billet } from './entities/billet';
 import { IBillet } from './interfaces/Billet';
 import { BilletStatus } from './enums/billet-status';
 import { CPF } from './valueObjects/cpf';
+import { BilletService } from './services/billet.service';
+
+class MockBilletService extends BilletService {
+  async create(createBilletDto: CreateBillet): Promise<Billet> {
+    const mockBilletPayment: IBillet = {
+      id: '1',
+      createdAt: new Date().toISOString(),
+      fee: 0,
+      status: BilletStatus.created,
+      ...createBilletDto,
+    };
+
+    return new Billet(mockBilletPayment);
+  }
+}
 
 describe('BilletController', () => {
   let billetController: BilletController;
@@ -12,6 +27,7 @@ describe('BilletController', () => {
   beforeEach(async () => {
     const app = await Test.createTestingModule({
       controllers: [BilletController],
+      providers: [{ provide: 'BilletService', useClass: MockBilletService }],
     }).compile();
 
     billetController = app.get<BilletController>(BilletController);
@@ -19,6 +35,8 @@ describe('BilletController', () => {
 
   describe('#create', () => {
     it('Should return a billet when create is successful', async () => {
+      jest.useFakeTimers({ now: new Date('2020-04-24T01:33:40.611174+00:00') });
+
       const mockedBilletInput: CreateBillet = {
         line: '123454',
         barCode: '1234556',
@@ -26,22 +44,24 @@ describe('BilletController', () => {
         description: "I'm a mocked billet",
         amountInCents: 5000,
         tags: [],
-        scheduledDate: '2020-04-24T01:33:40.611174+00:00',
+        scheduledDate: '2020-04-25',
       };
 
       const result = await billetController.create(mockedBilletInput);
 
       const mockBilletPayment: IBillet = {
         id: '1',
-        ...mockedBilletInput,
-        createdAt: '2020-04-24T01:33:40.611174+00:00',
+        createdAt: new Date().toISOString(),
         fee: 0,
         status: BilletStatus.created,
+        ...mockedBilletInput,
       };
 
       const expected = new Billet(mockBilletPayment);
 
       expect(result).toEqual(expected);
+
+      jest.useRealTimers();
     });
   });
 });
--- src/billet/billet.controller.ts
+++ src/billet/billet.controller.ts
@@ -1,23 +1,16 @@
-import { Controller, Body, Post } from '@nestjs/common';
+import { Controller, Body, Post, Inject } from '@nestjs/common';
 import { CreateBillet } from './dto/create-billet.dto';
 import { Billet } from './entities/billet';
-import { IBillet } from './interfaces/Billet';
-import { BilletStatus } from './enums/billet-status';
+import { BilletService } from './services/billet.service';
 
 @Controller('billet')
 export class BilletController {
-  constructor() {}
+  constructor(
+    @Inject('BilletService') private readonly billetService: BilletService,
+  ) {}
 
   @Post()
   async create(@Body() createBilletDto: CreateBillet): Promise<Billet> {
-    const billet: IBillet = {
-      id: '1',
-      ...createBilletDto,
-      createdAt: '2020-04-24T01:33:40.611174+00:00',
-      fee: 0,
-      status: BilletStatus.created,
-    };
-
-    return new Billet(billet);
+    return this.billetService.create(createBilletDto);
   }
 }
